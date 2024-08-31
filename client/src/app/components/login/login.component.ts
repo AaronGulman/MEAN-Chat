15addfc2d7515ee49af80a87606e8a5d4288effc
Draco Zhang <1092469607@qq.com>
2024-08-31 22:06:03 +1000

    Added client login/register UI

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,12 +1,23 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service'
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  imports: [CommonModule,FormsModule,RouterModule],
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+  }
 }
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  standalone: true,
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule]
 })
 export class RegisterComponent {
+  username: string = '';
+  email: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService) {}
+
+  onRegister() {
+
+  }
 }
