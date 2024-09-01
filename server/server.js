711f0f1adb5d28448f6491646e2b34a76f4e34cc
Draco Zhang <1092469607@qq.com>
2024-09-02 00:59:09 +1000

    Setup server login/register POST routes

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const authRoutes = require('./routes/auth.routes.js');
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.use(authRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
